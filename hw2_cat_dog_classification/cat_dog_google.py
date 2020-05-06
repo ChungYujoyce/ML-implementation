b1256e361dafe6800e0755b4de50e568e7fc83e4
graceHunag <joyce.bhps@gmail.com>
2020-05-06 23:53:20 +0800

    use pretrained model "inceptionV3" acc=90 up

---
+++ hw2_cat_dog_classification/cat_dog_google.py
@@ -0,0 +1,128 @@
+'''
+Reference: Google ML tutorial
+
+  we will use the Inception V3 model developed at Google, 
+  and pre-trained on ImageNet, a large dataset of web images (1.4M images and 1000 classes)
+'''
+import os
+
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+local_weights_file = './inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5'
+pre_trained_model = InceptionV3(
+    input_shape=(150, 150, 3), include_top=False, weights=None)
+pre_trained_model.load_weights(local_weights_file)
+for layer in pre_trained_model.layers:
+    layer.trainable = False
+
+last_layer = pre_trained_model.get_layer('mixed7')
+print('last layer output shape:', last_layer.output_shape)
+last_output = last_layer.output
+
+# Flatten the output layer to 1 dimension
+x = layers.Flatten()(last_output)
+# Add a fully connected layer with 1,024 hidden units and ReLU activation
+x = layers.Dense(1024, activation='relu')(x)
+# Add a dropout rate of 0.2
+x = layers.Dropout(0.2)(x)
+# Add a final sigmoid layer for classification
+x = layers.Dense(1, activation='sigmoid')(x)
+
+# Configure and compile the model
+model = Model(pre_trained_model.input, x)
+
+base_dir = 'cats_and_dogs_small'
+
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+train_cats_dir = os.path.join(train_dir, 'cats')
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+test_cats_dir = os.path.join(test_dir, 'cats')
+test_dogs_dir = os.path.join(test_dir, 'dogs')
+
+# Add our data-augmentation parameters to ImageDataGenerator
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+# Note that the validation data should not be augmented!
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir, # This is the source directory for training images
+        target_size=(150, 150),  # All images will be resized to 150x150
+        batch_size=20,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+# Flow validation images in batches of 20 using val_datagen generator
+validation_generator = val_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+
+from tensorflow.keras.optimizers import SGD
+
+unfreeze = False
+
+# Unfreeze all models after "mixed6"
+for layer in pre_trained_model.layers:
+  if unfreeze:
+    layer.trainable = True
+  if layer.name == 'mixed6':
+    unfreeze = True
+
+# As an optimizer, here we will use SGD 
+# with a very low learning rate (0.00001)
+model.compile(loss='binary_crossentropy',
+              optimizer=SGD(
+                  lr=0.00001, 
+                  momentum=0.9),
+              metrics=['acc'])
+
+callbacks = []
+#callbacks.append(tf.keras.callbacks.ModelCheckpoint('./checkpoint.h5', save_best_only=True, save_weights_only=False))
+
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=100,
+      epochs=50,
+      validation_data=validation_generator,
+      validation_steps=50,
+      verbose=2)
+
+# Plot training history
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.savefig('acc.png')
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.savefig('loss.png')
